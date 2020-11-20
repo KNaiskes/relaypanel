a56e662193ac5d562575187ed411246992d65c52
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-11-20 10:44:19 +0200

    Add Relay model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Relay(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    device = models.CharField(max_length=15)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
