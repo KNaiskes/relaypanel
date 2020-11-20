cb889a63db7eb3176d9111ebdf77fbeb0f576b9b
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-11-20 12:33:49 +0200

    Add api_view relays in order to get all available relays

--- api/views.py
+++ api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+from .models import Relay
+from .serializers import RelaySerializer
+
+@api_view(['GET'])
+def relays(request):
+    if request.method == 'GET':
+        relays_list = Relay.objects.all()
+        serializer = RelaySerializer(relays_list,
+                        context={'request': request}, many=True)
+
+        return Response({'relays': serializer.data})
